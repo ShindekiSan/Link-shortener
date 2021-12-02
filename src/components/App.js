15f956c06035438b000c2803aff02e383f4b3b64
ShindekiSan <abelchuk.maksim@gmail.com>
2021-12-02 15:05:16 +0300

    webpack config and chunks splitting

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import LogIn from './AuthPage/LogIn';
-import SignUp from './AuthPage/SignUp';
-import MainPage from './MainPage/MainPage';
-import Profile from './ProfilePage/Profile';
-import LinkDetails from './ProfilePage/LinkDetails'
+import React, { Suspense, lazy } from 'react';
+const LogIn = lazy(() => import('./AuthPage/LogIn'));
+const SignUp = lazy(() => import('./AuthPage/SignUp'));
+const MainPage = lazy(() => import('./MainPage/MainPage'));
+const Profile = lazy(() => import('./ProfilePage/Profile'));
+const LinkDetails = lazy(() => import('./ProfilePage/LinkDetails'));
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useAuth } from '../hooks/auth.hook';
 import { AuthContext } from '../context/AuthContext.js';
 import '../styles/app.css';
-import 'materialize-css';
 import '../styles/normalize.css';
-import ShortenerPage from './ShortenerPage/ShortenerPage';
-import SearchLinksPage from './SearchLinksPage/SearchLinksPage';
-import SearchedLinkDetails from './SearchLinksPage/SearchedLinkDetails'
+const ShortenerPage = lazy(() => import('./ShortenerPage/ShortenerPage'));
+const SearchLinksPage = lazy(() => import('./SearchLinksPage/SearchLinksPage'));
+const SearchedLinkDetails = lazy(() => import('./SearchLinksPage/SearchedLinkDetails'));
+import Loader from './UI/Loader';
 
 function App () {
     const {token, login, logout, userName, userId} = useAuth()
@@ -21,18 +21,20 @@ function App () {
         <AuthContext.Provider value={{
             userId, token, login, logout, userName, isAuthenticated
         }}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<MainPage />} />
-                    <Route path='/login' element={<LogIn />} />
-                    <Route path='/signup' element={<SignUp />} />
-                    <Route path='/profile' element={<Profile />} />
-                    <Route path='/link-detail/:id' element={<LinkDetails />} />
-                    <Route path='/shortener' element={<ShortenerPage />} />
-                    <Route path='/search' element={<SearchLinksPage />} />
-                    <Route path='/link-info/:id' element={<SearchedLinkDetails />} />
-                </Routes>
-            </BrowserRouter>
+            <Suspense fallback={<Loader />}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<MainPage />} />
+                        <Route path='/login' element={<LogIn />} />
+                        <Route path='/signup' element={<SignUp />} />
+                        <Route path='/profile' element={<Profile />} />
+                        <Route path='/link-detail/:id' element={<LinkDetails />} />
+                        <Route path='/shortener' element={<ShortenerPage />} />
+                        <Route path='/search' element={<SearchLinksPage />} />
+                        <Route path='/link-info/:id' element={<SearchedLinkDetails />} />
+                    </Routes>
+                </BrowserRouter>
+            </Suspense>
         </AuthContext.Provider>
     )
 }
