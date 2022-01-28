1d10c0c4b2a16d65822e31c5c5007f53c56dff91
ShindekiSan <abelchuk.maksim@gmail.com>
2022-01-28 11:14:36 +0300

    fixed baseUrl for new links

--- config/default.ts
+++ config/default.ts
@@ -2,7 +2,7 @@ const config = {
   port: process.env.PORT || 5000,
   mongoUri: process.env.MONGODB_URI || 'mongodb+srv://Maxim:a298925134@cluster0.stw0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   jwtSecret: 'test project',
-  baseUrl: 'http://localhost:5000',
+  baseUrl: 'https://links-shortener-api.herokuapp.com',
 };
 
 export default config;
